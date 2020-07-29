a941be5355bc59f516b7648b1508007f8a937759
knightvertrag <amiapu1@gmail.com>
2020-07-30 04:39:55 +0530

    Main body component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./Main.module.scss";
+import stays from "../../Data/stays.json";
+import StayCard, { IStay } from "../StayCard/StayCard";
+
+const Main = () => {
+  return <div></div>;
+};
+
+export default Main;
