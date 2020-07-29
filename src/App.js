a67e864cfd9158841f18da3d001ab28fe2380c25
knightvertrag <amiapu1@gmail.com>
2020-07-30 05:13:44 +0530

    Added Main section to root

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./App.module.scss";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return (
     <div className={classes.content_area}>
       <Header />
+      <Main />
     </div>
   );
 };
