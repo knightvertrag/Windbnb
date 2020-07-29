1a4b4c2860aa628c25e75efc4f075da05e3b6841
knightvertrag <amiapu1@gmail.com>
2020-07-30 02:47:41 +0530

    Added basic header ui

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React, { Fragment, useState } from "react";
+import classes from "./Header.module.scss";
+import Logo from "../../assets/logo.svg";
+import { Search } from "./Search/Search";
+const Header = () => {
+  const [filterShow, setFilterShow] = useState(false);
+
+  return (
+    <Fragment>
+      <div className={classes.header}>
+        <img src={Logo} alt="logo" className={classes.logo} />
+        <Search />
+      </div>
+      <div className={classes.info}>
+        <div className={classes.stays}>Stays in Finland</div>
+        <div className={classes.amnt}>12+ Stays</div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
