4325a0a9d14bc8e6cb31e4edd34968454526cf0e
knightvertrag <amiapu1@gmail.com>
2020-07-31 21:33:22 +0530

    Added props

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import classes from "./Header.module.scss";
 import Logo from "../../assets/logo.svg";
 import { Search } from "./Search/Search";
+import Filter from "./Filter/Filter";
+
 const Header = () => {
+  const [filterShow, setFilterShow] = useState(false);
+
+  if (filterShow) {
+    return <Filter />;
+  }
   return (
     <Fragment>
       <div className={classes.header}>
         <img src={Logo} alt="logo" className={classes.logo} />
-        <Search />
+        <Search filterShow={filterShow} setFilterShow={setFilterShow} />
       </div>
       <div className={classes.info}>
         <div className={classes.stays}>Stays in Finland</div>
