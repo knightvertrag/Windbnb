1847e7bc3f1024d6c498029bb464a16541a11973
knightvertrag <amiapu1@gmail.com>
2020-08-01 01:51:46 +0530

    Added filter skeleton

--- src/components/Header/Filter/Filter.jsx
+++ src/components/Header/Filter/Filter.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import classes from "./Filter.module.scss";
+import Button from "../../Util/Button/Button";
 
 const Filter = () => {
   return (
     <div className={classes.container}>
       <div className={classes.categories}>
-        <div className={classes.location}>Location</div>
-        <div className={classes.guests}>Guests</div>
-        <div className={classes.btn}>Search</div>
+        <div className={classes.location}>
+          <div>location search field</div>
+          <div>options</div>
+        </div>
+        <div className={classes.guests}>
+          <div>Guest search field</div>
+          <div>options</div>
+        </div>
+        <div className={classes.btn}>
+          <Button icon={`search`}>Search</Button>
+        </div>
       </div>
     </div>
   );
