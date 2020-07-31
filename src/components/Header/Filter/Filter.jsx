aa23d0c7af34d83e2250c41af0c21127f398864a
knightvertrag <amiapu1@gmail.com>
2020-07-31 22:15:30 +0530

    Filter component set up

--- src/components/Header/Filter/Filter.jsx
+++ src/components/Header/Filter/Filter.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import classes from "./Filter.module.scss";
 
 const Filter = () => {
-  return <div></div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.categories}>
+        <div className={classes.location}>Location</div>
+        <div className={classes.guests}>Guests</div>
+        <div className={classes.btn}>Search</div>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
