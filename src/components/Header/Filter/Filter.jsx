aa5d705c6b00211fded9041da969d1203594722b
knightvertrag <amiapu1@gmail.com>
2020-08-02 01:08:41 +0530

    Added backdrop

--- src/components/Header/Filter/Filter.jsx
+++ src/components/Header/Filter/Filter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classes from "./Filter.module.scss";
 import Button from "../../Util/Button/Button";
 import Input from "./Input/Input";
@@ -33,28 +33,33 @@ const Filter = ({ setFilterShow }) => {
   };
 
   return (
-    <div className={classes.container}>
-      <i
-        className={`material-icons ${classes.back}`}
-        onClick={() => setFilterShow(false)}
-      >
-        arrow_back
-      </i>
-      <div className={classes.categories}>
-        <div className={classes.location_container}>
-          <Input type={`Location`} placeholder={`Add Location`} />
-          {showLocations()}
-        </div>
-        <div className={classes.guests}>
-          <Input type={`Guests`} placeholder={`Add Guests`} />
-          <div>options</div>
-        </div>
-        <div className={classes.btn}>
-          <Button icon={`search`}>Search</Button>
+    <Fragment>
+      <div className={classes.container}>
+        <i
+          className={`material-icons ${classes.back}`}
+          onClick={() => setFilterShow(false)}
+        >
+          arrow_back
+        </i>
+        <div className={classes.categories}>
+          <div className={classes.location_container}>
+            <Input type={`Location`} placeholder={`Add Location`} />
+            {showLocations()}
+          </div>
+          <div className={classes.guests}>
+            <Input type={`Guests`} placeholder={`Add Guests`} />
+            <div>options</div>
+          </div>
+          <div className={classes.btn}>
+            <Button icon={`search`}>Search</Button>
+          </div>
         </div>
       </div>
-      <div className={classes.backdrop}></div>
-    </div>
+      <div
+        className={classes.backdrop}
+        onClick={() => setFilterShow(false)}
+      ></div>
+    </Fragment>
   );
 };
 
