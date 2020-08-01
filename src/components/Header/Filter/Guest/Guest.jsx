6ba0770422ad481238223806c5ec8d9bbc105e05
knightvertrag <amiapu1@gmail.com>
2020-08-02 05:06:20 +0530

    Guest filter element

---
+++ src/components/Header/Filter/Guest/Guest.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Guest.module.scss";
+
+const Guest = ({ title }) => {
+  return <div className={classes.container}></div>;
+};
+
+export default Guest;
