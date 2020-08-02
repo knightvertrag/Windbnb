41a6e09c4da49602cecfe12734d239f89a916d50
knightvertrag <amiapu1@gmail.com>
2020-08-03 03:51:02 +0530

    Search bar shows current selected location

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import classes from "./Search.module.scss";
 
-export const Search = ({ filterShow, setFilterShow }) => {
+export const Search = ({
+  filterShow,
+  setFilterShow,
+  location,
+  setLocation,
+}) => {
   const onClick = () => {
     setFilterShow(true);
   };
 
   return (
     <div className={classes.container}>
-      <div className={classes.location}>Helsinki, Finland</div>
+      <div
+        className={classes.location}
+      >{`${location.city}, ${location.country}`}</div>
       <div className={classes.guest}>Add Guests</div>
       <i className={`material-icons ${classes.icon}`} onClick={() => onClick()}>
         search
