90ff9c4adc54ec5d384f2d080c20873d1f2ffded
knightvertrag <amiapu1@gmail.com>
2020-08-02 05:55:03 +0530

    Added state for adult, children counts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./App.module.scss";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 const App = () => {
+  const [adult, setAdult] = useState({
+    amount: 0,
+  });
+  const [child, setChild] = useState({
+    amount: 0,
+  });
   return (
     <div className={classes.content_area}>
       <Header />
