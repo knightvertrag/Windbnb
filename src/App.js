8e635964ab0b828d5cbb731373a2bbebc3692939
knightvertrag <amiapu1@gmail.com>
2021-01-20 02:26:29 +0530

    Added a header image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import classes from "./App.module.scss";
 import Main from "./components/Main/Main";
+import image from './assets/head.jpg'
 
 const App = () => {
   return (
     <div className={classes.content_area}>
+      <img src={image} alt="Banner" className={classes.img} />
       <Main/>
       <hr />
       <div className={classes.footer}>GNU/Linux Users' Group, NIT Durgapur.</div>
